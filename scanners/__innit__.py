3e686c8dc54e7323e89300cf20f2b55d574ad638
credibility-index <nothetal@gmail.com>
2026-01-27 17:51:22 +0300

    Update __innit__.py

--- scanners/__innit__.py
+++ scanners/__innit__.py
@@ -1,16 +1,1 @@
-# scanners/__init__.py
-"""
-WebSecAI Security Scanners Package
-"""
 
-from .sql_scanner import scan_sql_injection
-from .xss import scan_xss
-from .csrf_scanner import check_csrf_protection
-from .ssrf_scanner import scan_ssrf
-
-__all__ = [
-    'scan_sql_injection',
-    'scan_xss', 
-    'check_csrf_protection',
-    'scan_ssrf'
-]
